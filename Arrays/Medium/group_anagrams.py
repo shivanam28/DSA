e7e5a99a4b029486426ef2f42a7ebbedffa5893e
Shivani Singh <anjushivi2004@gmail.com>
2026-08-18 16:04:32 +0530

    Hashing Questions Solved - 18Aug2026

---
+++ Arrays/Medium/group_anagrams.py
@@ -0,0 +1,30 @@
+'''
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+'''
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        groups = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+
+            if key not in  groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return list(groups.values())
