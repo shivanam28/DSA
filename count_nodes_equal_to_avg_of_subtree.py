2644f26de4c32402233f3dc3bd4f35637236ca4d
Shivani Singh <anjushivi2004@gmail.com>
2026-09-11 18:34:04 +0530

    Subtree Question Solved

---
+++ count_nodes_equal_to_avg_of_subtree.py
@@ -0,0 +1,50 @@
+'''
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+
+Note:
+The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+A subtree of root is a tree consisting of root and all of its descendants.
+ 
+
+Example 1:
+Input: root = [4,8,5,0,1,null,6]
+Output: 5
+Explanation: 
+For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+For the node with value 0: The average of its subtree is 0 / 1 = 0.
+For the node with value 1: The average of its subtree is 1 / 1 = 1.
+For the node with value 6: The average of its subtree is 6 / 1 = 6.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def averageOfSubtree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(node):
+            if node is None:
+                return 0,0,0
+
+            left_sum, left_count, left_ans = dfs(node.left)
+            right_sum, right_count, right_ans = dfs(node.right)
+
+            total_sum = node.val + left_sum + right_sum
+            total_count = 1+ left_count + right_count
+
+            ans = left_ans + right_ans
+
+            if node.val == total_sum // total_count:
+                ans +=1
+            
+            return total_sum, total_count, ans
+        
+        sum, count, ans = dfs(root)
+        return ans
